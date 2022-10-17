2ab40d92d565b8507f566f79d1a383ae04dc2428
RutujaPatil18 <prutuja189@gmail.com>
2022-10-17 19:55:26 +0530

    Initial commit

---
+++ src/main/java/com/tictactoe/App.java
@@ -0,0 +1,14 @@
+package com.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to  Tic Tac Toe Game!" );
+        
+    }
+}
