17c1a903ade5fa09e7b4e71d05f27f5aa6b9da0b
RutujaPatil18 <prutuja189@gmail.com>
2022-10-17 20:54:09 +0530

    Add: Ability to allow player to choose a letter X or O

--- src/main/java/com/tictactoe/App.java
+++ src/main/java/com/tictactoe/App.java
@@ -4,14 +4,13 @@ package com.tictactoe;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to  Tic Tac Toe Game!" );
-        TicTacToeGame ticTacToeGame=new TicTacToeGame();
-        ticTacToeGame.boardGame();
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to  Tic Tac Toe Game!");
+		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		ticTacToeGame.boardGame();
+		ticTacToeGame.choiceOfPlayer();
+		
 
-
-    }
+	}
 }
--- src/main/java/com/tictactoe/TicTacToeGame.java
+++ src/main/java/com/tictactoe/TicTacToeGame.java
@@ -1,13 +1,36 @@
 package com.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	char[] board=new char[10];
+	// create array of size 10
+	Scanner scanner = new Scanner(System.in);
+	char[] board = new char[10];
+	char computer, player;
 
+	// create board assign value to board
 	public void boardGame() {
-		for (int boardValue = 1; boardValue < board.length; boardValue++) {
-           board [boardValue] =' ';
-           System.out.println(board [boardValue]);
+		for (int boardValue = 0; boardValue < board.length; boardValue++) {
+			board[boardValue+1] = ' ';// Assign value to board
 
+		}
+	}
+    //player's choice X or O
+	public void choiceOfPlayer() {
+		System.out.println("Player to choose a letter X or O");
+		char option = scanner.next().charAt(0);
+		switch (option) {
+		case 'X':
+			player = 'X';
+			computer = 'O';
+			break;
+		case 'O':
+			player = 'O';
+			computer = 'X';
+			break;
+		default:
+			System.out.println("Invalid Intput");
+			break;
 		}
 	}
 }
